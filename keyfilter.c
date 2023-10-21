4a678efc92dbc5fab6cf5479fe001a2229211d12
Kristian-045 <kaleta.kristian@gmail.com>
2023-10-21 14:19:03 +0200

    add end of string character

--- keyfilter.c
+++ keyfilter.c
@@ -96,6 +96,8 @@ int main(int argc, char *argv[]) {
                 sorterResult[resultIndex++] = (char) i;
             }
         }
+        sorterResult[resultIndex]='\0'
+
         printf("Enable: %s", sorterResult);
     }
 
